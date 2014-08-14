249418dde88f33717cfb9de9e9c9ce05d503db8d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-14 18:18:04 +0200

    Remove dead code

--- browser-pack.el
+++ browser-pack.el
@@ -12,9 +12,6 @@
 (setq browse-url-browser-function 'browse-url-firefox
       browse-url-generic-program "/usr/bin/firefox")
 
-;; (setq browse-url-browser-function 'browse-url-generic
-;;       browse-url-generic-program "/usr/bin/google-chrome"
-;;       browse-url-generic-args '("--no-proxy-server"))
 
 (provide 'browser-pack)
 ;;; browser-pack.el ends here
